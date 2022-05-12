988099edfc8c316795e24f12f07f4724c4e50556
ThodorisKor <thodoris6957541943@gmail.com>
2022-05-12 16:30:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/travelagency/ui/form/form.java
@@ -0,0 +1,37 @@
+package com.example.travelagency.ui.form;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.travelagency.databinding.FragmentPackagesBinding;
+
+public class form extends Fragment {
+
+    private FragmentPackagesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // PackagesViewModel slideshowViewModel =
+        //new ViewModelProvider(this).get(PackagesViewModel.class);
+
+        binding = FragmentPackagesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.textSlideshow;
+        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
