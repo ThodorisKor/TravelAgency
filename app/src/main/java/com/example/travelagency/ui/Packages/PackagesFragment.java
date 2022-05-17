2a0076f233896dd48f7025a2a92e2347b9820d1f
ThodorisKor <thodoris6957541943@gmail.com>
2022-05-17 19:59:16 +0300

    Initial commit

--- app/src/main/java/com/example/travelagency/ui/Packages/PackagesFragment.java
+++ app/src/main/java/com/example/travelagency/ui/Packages/PackagesFragment.java
@@ -1,34 +1,120 @@
 package com.example.travelagency.ui.Packages;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.travelagency.MainActivity;
+import com.example.travelagency.R;
+
+import com.example.travelagency.databinding.FragmentFormBinding;
 import com.example.travelagency.databinding.FragmentPackagesBinding;
+import com.example.travelagency.ui.form.form;
 
 public class PackagesFragment extends Fragment {
-
     private FragmentPackagesBinding binding;
-
+    private CardView card1;
+    private CardView card2;
+    private CardView card3;
+    private CardView card4;
+    private CardView card5;
+    private CardView card6;
+    private CardView card7;
+    private CardView card8;
+    private CardView card9;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       // PackagesViewModel slideshowViewModel =
-                //new ViewModelProvider(this).get(PackagesViewModel.class);
 
         binding = FragmentPackagesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        /** Below ,  we make a cardView , thats takes the cardView with id 'card1'
+         * We set a onclick listener , that when the user clicks inside the 'card1'
+         * To activate the listener and we call the anonymous method "onClick(View view)"
+         * And inside her we create a form Fragment with the name 'frag'
+         * And we call the method "replaceFragment('Fragment_Name')" and replaces the current
+         * Fragment with the Fragment we want to saw inside the container.Simple */
 
-        //final TextView textView = binding.textSlideshow;
-        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        card1 = binding.card1;
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getActivity(),cardView.isPressed(),Toast.LENGTH_SHORT).show();
+                System.out.println(card1.isPressed());
+                Fragment frag = new form();
+                replaceFragment(frag);
+                //FragmentFormBinding.bind(View.inflate(bd.navForm,R.layout.fragment_form, M\));
+            }
+        });
+
+        /**
+         * CAREFUL : binding.<the_id_of_CardView>
+         *     with the "binding" we connect the element of .xml to the .java file(i'm NOT 100% sure tho)*/
+        card2 = binding.card2;
+        card2.setOnClickListener(view -> {
+            System.out.println(card2.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card3 = binding.card3;
+        card3.setOnClickListener(view -> {
+            System.out.println(card3.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card4 = binding.card4;
+        card4.setOnClickListener(view -> {
+            System.out.println(card4.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card5 = binding.card5;
+        card5.setOnClickListener(view -> {
+            System.out.println(card5.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card6 = binding.card6;
+        card6.setOnClickListener(view -> {
+            System.out.println(card6.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card7 = binding.card7;
+        card7.setOnClickListener(view -> {
+            System.out.println(card7.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card8 = binding.card8;
+        card8.setOnClickListener(view -> {
+            System.out.println(card8.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
+        card9 = binding.card9;
+        card9.setOnClickListener(view -> {
+            System.out.println(card9.isPressed());
+            Fragment frag = new form();
+            replaceFragment(frag);
+        });
         return root;
     }
-
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment_content_main, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/travelagency/ui/form/form.java
+++ app/src/main/java/com/example/travelagency/ui/form/form.java
@@ -1,31 +1,33 @@
 package com.example.travelagency.ui.form;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.travelagency.R;
+import com.example.travelagency.databinding.FragmentFormBinding;
 import com.example.travelagency.databinding.FragmentPackagesBinding;
 
 public class form extends Fragment {
 
-    private FragmentPackagesBinding binding;
-
+    private FragmentFormBinding binding;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        // PackagesViewModel slideshowViewModel =
-        //new ViewModelProvider(this).get(PackagesViewModel.class);
 
-        binding = FragmentPackagesBinding.inflate(inflater, container, false);
+        binding = FragmentFormBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        //final TextView textView = binding.textSlideshow;
-        //slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Toast.makeText(getActivity(),"MIAOU",Toast.LENGTH_SHORT).show();
         return root;
     }
 
